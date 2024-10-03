1f90f5b4bceb72206b5cd770a269eceb5f1f8e6c
Purva <purva@10.1.85.179>
2024-10-03 18:41:17 -0500

    Covered annotation and life cycle of servlet

---
+++ WelcomeServletUsingAnnotation/src/main/java/com/learn/WelcomeServlet.java
@@ -0,0 +1,38 @@
+package com.learn;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/annotation")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String var1 = request.getParameter("var1");
+		String var2 = request.getParameter("var2");
+		String var3 = request.getParameter("var3");
+		response.getWriter().append("Welcome: "+var1).append(var2).append(var3).close();
+	}
+
+	
+
+}
